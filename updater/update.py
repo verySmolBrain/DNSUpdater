7b68db4b4170775fd9108209d62ce34c59bd8bd2
smolbrain <randomlyrandamu@gmail.com>
2023-05-13 00:31:59 +1000

    Rewrite

--- updater/update.py
+++ updater/update.py
@@ -1,91 +1,108 @@
 #!/usr/bin/env python3
+from typing import List
+from pydantic import BaseModel
 import logging 
 import threading 
 import subprocess
 import requests
 import time 
 import os 
+import toml
+from pathlib import Path
 
-def setupEnvironment():
-    REQUIRED_ENV_VARS = [
-        "TTL", 
-        "USERNAME",
-        "PASSWORD",
-        "DOMAIN", 
-        "SUBDOMAIN",
-    ]
+class AuthDetails(BaseModel):
+    username: str
+    password: str
+    subdomain: str
+    domain: str
 
-    for env_var in REQUIRED_ENV_VARS:
-        if not os.environ.get(env_var):
-            logging.critical(f"Please set the {env_var} environment variable")
+class DNSUpdaterConfig(BaseModel):
+    ttl: int
+    auth_details: List[AuthDetails]
+
+class DNSUpdater:
+    '''
+    A class for handling DNS updates.
+    '''
+    
+    def __init__(self):
+        toml_file = Path("auth_details.toml")
+        with open(toml_file, "r") as f:
+            toml_data = toml.load(f)
+        
+        self.config = DNSUpdaterConfig(**toml_data)
+        self.load_logger()
+    
+    def load_logger(self):
+        cur_time = time.strftime("%Y_%m_%d")
+        log_name = f"logs/{cur_time}"
+        
+        if not os.path.exists("logs"):
+            os.makedirs("logs")
+        if not os.path.exists(log_name):
+            open(log_name, "w").close()
+        
+        if len(os.listdir("logs")) > 1:
+            oldest_file = min(os.listdir("logs"))
+            os.remove(f"logs/{oldest_file}")
+        
+        logging.basicConfig(
+            format = '%(asctime)s | %(levelname)s: %(message)s', 
+            filemode = 'a',
+            filename = log_name,
+            level = logging.INFO
+        )
+    
+    def check_ip_requires_update(self, subdomain, domain):
+        target_ip = subprocess.check_output(
+            ["dig", "+short", f"{subdomain}.{domain}"]
+        ).decode("utf-8").strip()
+        
+        my_ip = requests.get("https://domains.google.com/checkip").text
+
+        logging.info(f"Domain name is {subdomain}.{domain} with IP {target_ip}")
+        logging.info(f"Host machine ip is {my_ip}")
+
+        if target_ip != my_ip:
+            logging.info("IPs do not match, updating...")
+            return True 
+        else:
+            logging.info("IPs match, no update needed")
+            return False
+    
+    def update_ip(self, username, password, domain, subdomain):
+        resp = requests.get(
+            "https://domains.google.com/nic/update",
+            params = {
+                "hostname": f"{subdomain}.{domain}",
+                "myip": requests.get("https://domains.google.com/checkip").text,
+            },
+            auth = (
+                username,
+                password,
+            ),
+        )
+
+        if resp.status_code == 200:
+            logging.info(f"Request success with message: {resp.content}")
+        else:
+            logging.error(f"Request failed with error {resp.content}")
             exit(1)
-
-    if int(os.environ.get("TTL")) < 900:
-        logging.critical(
-            f"Please set TTL to at least 900 seconds, currently set to {os.environ.get('TTL')}")
     
-    if not os.path.exists("logs"):
-        os.makedirs("logs")
-    while len(os.listdir("logs")) > 3:
-        os.remove("logs/" + os.listdir("logs")[0])
+    def run_update(self):
+        self.load_logger()
+        
+        for (username, password, domain, subdomain) in self.config.auth_details:
+            if self.check_ip_requires_update(subdomain, domain):
+                self.update_ip(username, password, domain, subdomain)
+        
+        logging.info(f"Sleeping for {self.config.ttl} seconds")
+        threading.Timer(self.config.ttl, self.run_update).start()
 
-    curTime = time.strftime("%Y_%m_%d")
-    logName = f"logs/{curTime}"
-    logging.basicConfig(
-        format = '%(asctime)s | %(levelname)s: %(message)s', 
-        filemode = 'a',
-        filename = logName,
-        level = logging.INFO
-    )
-
-def IPRequireUpdate():
-    SUBDOMAIN = os.environ.get("SUBDOMAIN")
-    DOMAIN = os.environ.get("DOMAIN")
-
-    targetIP = subprocess.check_output(
-        ["dig", "+short", f"{SUBDOMAIN}.{DOMAIN}"]
-    ).decode("utf-8").strip()
-    
-    myIP = requests.get("https://domains.google.com/checkip").text
-
-    logging.info(f"Domain name is {SUBDOMAIN}.{DOMAIN} with IP {targetIP}")
-    logging.info(f"Host machine ip is {myIP}")
-
-    if targetIP != myIP:
-        logging.info("IPs do not match, updating...")
-        return True 
-    else:
-        logging.info("IPs match, no update needed")
-        return False
-
-def updateIP():
-    resp = requests.get(
-        "https://domains.google.com/nic/update",
-        params = {
-            "hostname": f"{os.environ.get('SUBDOMAIN')}.{os.environ.get('DOMAIN')}",
-            "myip": requests.get("https://domains.google.com/checkip").text,
-        },
-        auth = (
-            os.environ.get("USERNAME"),
-            os.environ.get("PASSWORD"),
-        ),
-    )
-
-    if resp.status_code == 200:
-        logging.info("Request successful")
-    else:
-        logging.error(f"Request failed with error {resp.content}")
-        exit(1)
 
 def main():
-    setupEnvironment()
-    if IPRequireUpdate():
-        updateIP()
-
-    TTL = int(os.environ.get("TTL"))
-    logging.info(f"Sleeping for {TTL} seconds")
-
-    threading.Timer(TTL, main).start()
+    updater = DNSUpdater()
+    updater.run_update()
 
 if __name__ == "__main__":
     main()
